81843f8099f75aeae1eeb78828e90a666f7ef5c8
Zach Eriksen <zmeriksen@icloud.com>
2022-03-01 17:59:38 -0700

    Update tests for Cache and global cache

--- Tests/cTests/cTests.swift
+++ Tests/cTests/cTests.swift
@@ -6,7 +6,7 @@ final class cTests: XCTestCase {
     func testExample() throws {
         XCTAssert(
             t.suite {
-                try t.expect("that a basic transformation") {
+                try t.expect("that a basic transformation works") {
                     let transformer = c.transformer(
                         from: { string in Int(string) },
                         to: { int in "\(String(describing: int))" }
@@ -19,15 +19,57 @@ final class cTests: XCTestCase {
                 }
                 
                 try t.expect("that a cache works") {
-                    c.set(value: Double.pi, forKey: "🥧")
+                    let cache = c.cache()
                     
-                    let pi: Double = c.get("🥧") ?? 0
+                    cache.set(value: Double.pi, forKey: "🥧")
+                    
+                    let pi: Double = cache.get("🥧") ?? 0
                     
                     try t.assert(pi, isEqualTo: .pi)
                     
-                    let resolvedValue: Double = c.resolve("🥧")
+                    let resolvedValue: Double = cache.resolve("🥧")
                     
                     try t.assert(resolvedValue, isEqualTo: .pi)
+                }
+                
+                try t.expect("that the global cache works") {
+                    
+                    try t.expect("that we can set and get a Cache") {
+                        c.set(
+                            value: .init(initialValues: [:]),
+                            forKey: "cache"
+                        )
+                        
+                        try t.assert(isNotNil: c.get("cache"))
+                    }
+                    
+                    try t.expect("that we can update a Cache and add a new one") {
+                        guard let cache = c.get("cache") else { throw t.error(description: "Could not find Cache.") }
+                        
+                        cache.set(value: "Hello, World!", forKey: "hello")
+                        
+                        let value: String? = cache.get("hello")
+                        
+                        try t.assert(isNotNil: value)
+                        try t.assert(value, isEqualTo: "Hello, World!")
+                        
+                        c.set(value: cache, forKey: "cache")
+                        
+                        c.set(
+                            value: .init(initialValues: ["hello": "Hi!"]),
+                            forKey: "cache2"
+                        )
+                    }
+                    
+                    try t.expect("that we can get both Caches") {
+                        guard
+                            let cache = c.get("cache"),
+                            let cache2 = c.get("cache2")
+                        else { throw t.error(description: "Missing a Cache.") }
+                        
+                        try t.assert(cache.get("hello"), isEqualTo: "Hello, World!")
+                        try t.assert(cache2.get("hello"), isEqualTo: "Hi!")
+                    }
                 }
             }
         )
