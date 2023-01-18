d6cde586a6ab05364277bb0f89a275bc5fe6d529
Leif <zmeriksen@icloud.com>
2023-01-17 19:50:39 -0700

    Bump supported platform versions

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "c",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v11),
-        .watchOS(.v7),
-        .tvOS(.v14)
+        .iOS(.v15),
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
